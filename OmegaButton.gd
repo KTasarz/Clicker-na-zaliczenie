ed1c5c38432c4f72d0af53a8a73f406361925400
KTasarz <KrystianTasarz01@gmail.com>
2026-03-19 12:19:54 +0100

    podstawowe funkcje gry
    dodanie guzika do zdobywania punktów
    dodanie tekstu pokazujące punkty
    dodanie placeholder tła

---
+++ OmegaButton.gd
@@ -0,0 +1,16 @@
+extends Button
+
+@onready var points = $"../Points"
+var times_pressed = 0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_pressed() -> void:
+	times_pressed = times_pressed + 1
+	points.text = "POINTS: " + str(times_pressed)
