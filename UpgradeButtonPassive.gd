9e09b887b4381f97ea64c508e88fa33e4af234e0
AntoniukL <lukasz.antoniuk85@gmail.com>
2026-04-16 01:29:16 +0200

    bug fix

--- UpgradeButtonPassive.gd
+++ UpgradeButtonPassive.gd
@@ -4,8 +4,8 @@ extends Button
 
 func _on_pressed() -> void:
 	print("Sprawdzanie czy "+str(Points.amount)+" >= "+ str(UpgradeManager.passive1.cost))
-	if Points.amount >= UpgradeManager.passive1cost:
+	if Points.amount >= UpgradeManager.passive1.cost:
 		print("Sprawdzenie udane")
-		Points.amount = Points.amount - UpgradeManager.passive1cost
+		Points.amount = Points.amount - UpgradeManager.passive1.cost
 		banner.text = "POINTS: " + str(Points.amount)
 		UpgradeManager.passive1.upgrade_passive(1)
